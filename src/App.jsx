75c6e6311f47e2012a15c28946b6adcebb4e811f
Jacques Farrell <129415702+Bangjekkk@users.noreply.github.com>
2024-12-22 23:55:54 +0700

    Add Hero Section & Projects

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hero from "./sections/Hero/Hero";
+import Project from "./sections/Projects/Project";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Project />
+    </>
+  );
+}
+
+export default App;
